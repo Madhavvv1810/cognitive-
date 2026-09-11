e48efd26dca9077cd462573325aa68da03d9fea8
Madhavvv1810 <opmadhav01@gmail.com>
2026-09-11 09:15:29 +0530

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -2,7 +2,7 @@
 L = [1,0,2,4,1,7,0,1,9,7]
 
 print(L)
-print([x*10 for x in L])
+print([no*10 for no in L])
 
 L += [1,8]
 L.insert(3,7)
@@ -17,22 +17,26 @@ print(L[-3:])
 
 avg = sum(L)/len(L)
 print(avg)
-print([x for x in L if x > avg])
+print([no for no in L if no > avg])
 
 
 # Q2
 scores = tuple(L[:8])
 
 print(scores)
-print(max(scores), scores.index(max(scores)))
-print(min(scores), scores.index(min(scores)))
-print(scores[::-1])
+print("Highest:", max(scores))
+print("Lowest:", min(scores))
+print("Reverse:", scores[::-1])
 
 x = int(input("Enter score: "))
-print(scores.index(x) if x in scores else "Not found")
 
-a,b,*c = scores
-print(a,b,c)
+if x in scores:
+    print("Index:", scores.index(x))
+else:
+    print("Not found")
+
+a, b, *c = scores
+print(a, b, c)
 
 
 # Q3
@@ -41,26 +45,39 @@ import random
 random.seed(1024170356)
 nums = [random.randint(100,900) for i in range(100)]
 
-even = [x for x in nums if x%2 == 0]
-odd = [x for x in nums if x%2 != 0]
+even = []
+odd = []
 
-print(even)
-print(odd)
+for x in nums:
+    if x % 2 == 0:
+        even.append(x)
+    else:
+        odd.append(x)
+
+print("Even:", even)
+print("Odd:", odd)
 
 def prime(n):
-    return n > 1 and all(n%i for i in range(2,n))
+    for i in range(2,n):
+        if n % i == 0:
+            return False
+    return n > 1
 
 p = [x for x in nums if prime(x)]
-print(p)
+print("Prime:", p)
 
-count = {x:nums.count(x) for x in set(nums)}
-x = max(count, key=count.get)
-print(x, count[x])
+count = {}
+for x in nums:
+    count[x] = count.get(x,0) + 1
+
+most = max(count, key=count.get)
+print("Most repeated:", most)
+print("Times:", count[most])
 
 
 # Q4
-A = {x*7 for x in [1,0,2,4,7]}
-B = {x*9 for x in [1,0,2,4,7]}
+A = {x*7 for x in [1,2,3,4,5]}
+B = {x*9 for x in [1,2,3,4,5]}
 
 print(A)
 print(B)
